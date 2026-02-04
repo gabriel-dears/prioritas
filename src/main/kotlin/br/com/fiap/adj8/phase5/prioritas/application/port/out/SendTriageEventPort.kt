e5995daf77b79b69508776de3889f255896051cd
Gabriel Soares <gabrielalcantarars@gmail.com>
2026-02-03 22:25:54 -0300

    feat: Updating TODO

--- src/main/kotlin/br/com/fiap/adj8/phase5/prioritas/application/port/out/SendTriageEventPort.kt
+++ src/main/kotlin/br/com/fiap/adj8/phase5/prioritas/application/port/out/SendTriageEventPort.kt
@@ -2,10 +2,9 @@ package br.com.fiap.adj8.phase5.prioritas.application.port.out
 
 import br.com.fiap.adj8.phase5.prioritas.common.event.TriageNotificationEvent
 
-// TODO: create common project for types and any other helper
-// Add new type below
-// use impl in the service
-// check if message has been successfully sent
+// TODO:
+// verify not only EMERGENCY SENT
+// create notification-app
 interface SendTriageEventPort {
     fun send(event: TriageNotificationEvent)
 }
